d6bc7844690d7745bb0e345aed071c8b4558b52f
Vitor Vianna <vitormatheusfv@gmail.com>
2023-04-10 21:06:31 -0300

    Second exercise with Set Lists

---
+++ src/lessons/Lesson07SetExercise02.java
@@ -0,0 +1,45 @@
+package lessons;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Lesson07SetExercise02 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many students for course A? ");
+        int students = sc.nextInt();
+        Set<Integer> cursoA = new TreeSet<>();
+        for (int i = 0; i < students; i++) {
+            cursoA.add(sc.nextInt());
+        }
+        System.out.print("How many students for course B? ");
+        students = sc.nextInt();
+        Set<Integer> cursoB = new TreeSet<>();
+        for (int i = 0; i < students; i++) {
+            cursoB.add(sc.nextInt());
+        }
+        System.out.print("How many students for course C? ");
+        students = sc.nextInt();
+        Set<Integer> cursoC = new TreeSet<>();
+        for (int i = 0; i < students; i++) {
+            cursoC.add(sc.nextInt());
+        }
+
+        Set<Integer> totalAlunos = new TreeSet<>(cursoA);
+        totalAlunos.addAll(cursoB);
+        totalAlunos.addAll(cursoC);
+
+        int sum = 0;
+        for (Integer alunos : totalAlunos) {
+            sum += 1;
+        }
+
+        System.out.println("Total de alunos matriculados: " + sum);
+
+    }
+}
